9040eb38605b950be5ce9f97608db1d33354ee56
codalo <ckodalo@gmail.com>
2022-09-03 00:49:12 +0300

    added like functionality

--- index.js
+++ index.js
@@ -2,22 +2,44 @@
 
 const cards = document.querySelector("#card-container");
 
-const likeCount = document.getElementsByClassName('likes')    
+  
 
+
+function addLiker() {
+  
+  for (let i = 0; i < cards.children.length; i++) {
+    let clickCount = 0;
+    cards.children[i].querySelector('#like-button').addEventListener('click', (e) => 
+    
+    {
+    const likeCount = cards.children[i].querySelector("#like-count")  
+    
+    console.log("I WS Clik")
+    console.log(likeCount);
+    clickCount += 1;
+    likeCount.innerText = `${clickCount} likes`;
+  
+  })
+}
+}
 
 // Function declarations
 
-function addLiker() {
-  var cardButtons = cards.querySelectorAll('#like-button')
-  cardButtons.forEach(() => {addEventListener('click', () => console.log("I WS CLIK"))
-})
-  //likebuttons.addEventListener('click', () => console.log("I WS CLIK"))
-    /*let clickCount = 0;
-    clickCount += 1
-    likeCount.textContent = clickCount;
-*/
-  }
+/*function addLiker() {
+  
+  for (let i = 0; i < cards.children.length; i++) {
+    
+    cards.children[i].querySelector('#like-button').addEventListener('click', (e) => 
+    
+    {console.log("I WS Clik")
+    console.log(likeCount);
+    clickCount ++
+    likeCount.innerText = `${clickCount} likes`;
+  
+  })
+}
 
+}*/
 function fetchToys () { 
 fetch('http://localhost:3000/toys')
         .then(res => res.json())
@@ -31,21 +53,33 @@ fetch('http://localhost:3000/toys')
               <p class="uid"> UID: ${uniqueId}</p> 
             </div>
             
-          <button id ="like-button" class="like-button">♥ 
-          <span id="like-count" class="likes">0 likes</span></button>
+          <button id ="like-button" class="like-button">♥</button> 
+          <span id ="like-count" class="likes">0 likes</span>
         </div>
-          </div>` 
-        
-         
+          </div>`
+          
+          
+          
         }
+         
+        addLiker();
+        console.log(cards); 
+        
+        
+        
+        
        
     
         
       })
         //document.getElementById('like-button').addEventListener('click', () => console.log("I WS CLIK"))
-    }
+        console.log(cards);
+       
+      }
 fetchToys();
-addLiker();
+
+
+
 
 
 
