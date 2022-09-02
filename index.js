dca2e5533e2cf270905fbb5f4ddd357a48e1f307
codalo <ckodalo@gmail.com>
2022-09-02 15:10:27 +0300

    done away with bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// Grabbing Elements Setting Global Vriables
+
 const cards = document.querySelector("#card-container");
 
+const likeCount = document.getElementsByClassName('likes')    
 
+
+// Function declarations
+
+function addLiker() {
+  var cardButtons = cards.querySelectorAll('#like-button')
+  cardButtons.forEach(() => {addEventListener('click', () => console.log("I WS CLIK"))
+})
+  //likebuttons.addEventListener('click', () => console.log("I WS CLIK"))
+    /*let clickCount = 0;
+    clickCount += 1
+    likeCount.textContent = clickCount;
+*/
+  }
 
 function fetchToys () { 
 fetch('http://localhost:3000/toys')
@@ -14,9 +30,22 @@ fetch('http://localhost:3000/toys')
               <p class="card-text">image no. ${i+1}</p>
               <p class="uid"> UID: ${uniqueId}</p> 
             </div>
-          </div>`  
-           }
-        })
-
+            
+          <button id ="like-button" class="like-button">♥ 
+          <span id="like-count" class="likes">0 likes</span></button>
+        </div>
+          </div>` 
+        
+         
+        }
+       
+    
+        
+      })
+        //document.getElementById('like-button').addEventListener('click', () => console.log("I WS CLIK"))
     }
 fetchToys();
+addLiker();
+
+
+
